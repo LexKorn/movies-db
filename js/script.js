6a92eb5c71fd5d99b396c51256001ad2b0f31713
Lex <fischer84@mail.ru>
2021-12-01 10:54:48 +0300

    add change tabs

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
 1) При установке галочки "Сделать любимым" - фильм выделить жирным зелёным;
 
 2) При кликание на Сериалы, Мультфильмы, Клипы - меняется постер и описание;
+
+3) Поиск среди просмотренных фильмов по 3 символам из строки поиска.
 */
 
 'use strict';
@@ -21,22 +23,20 @@ document.addEventListener('DOMContentLoaded', () => {
         };
         
     const adv = document.querySelector('.promo__adv'),
+          title = document.querySelector('.promo__title'),
           genre = document.querySelector('.promo__genre'),
+          descrab = document.querySelector('.promo__descr'),
           poster = document.querySelector('.promo__bg'),
           movieList = document.querySelector('.promo__interactive-list'),
           form = document.querySelector('.add'),
           input = document.querySelector('.adding__input'),
-          checkbox = document.querySelector('[type="checkbox"]');
+          checkbox = document.querySelector('[type="checkbox"]'),
+          tabs = document.querySelectorAll('.promo__menu-item');
     
-    adv.remove();
-    
-    genre.textContent = 'ДРАМА';
-    
-    poster.style.backgroundImage = 'url("img/serials.jpg")';
-
     let favoritFilm;
 
-        
+    adv.remove();
+  
     form.addEventListener('submit', (e) => {
         e.preventDefault();
 
@@ -87,4 +87,47 @@ document.addEventListener('DOMContentLoaded', () => {
         });                 
     }
     createMovieList(movieDB.movies, movieList);
+
+/////////////////
+
+    tabs.forEach((tab, i) => {
+        tab.addEventListener('click', () => {
+            tabs.forEach(tab => {
+                tab.classList.remove('promo__menu-item_active');
+            });            
+            tab.classList.add('promo__menu-item_active');
+            changeTab(i); 
+        });
+    });
+
+
+    function changeTab(index) {
+        switch (index) {
+            case 0: 
+                    title.textContent = 'МАРСИАНИН';
+                    genre.textContent = 'ДРАМА';
+                    descrab.textContent = 'ИСТОРИЯ ЧЕЛОВЕКА, ВЫЖИВШЕГО НА ЧУЖОЙ ПЛАНЕТЕ В ОДИНОЧКУ';
+                    poster.style.backgroundImage = 'url("img/bg.jpg")';
+                    break;
+            case 1: 
+                    title.textContent = 'ИНТЕРНЫ';
+                    genre.textContent = 'КОМЕДИЯ';
+                    descrab.textContent = 'Интерны, проходящие практику у неординарного заведующего отделением';
+                    poster.style.backgroundImage = 'url("img/serials.jpg")';
+                    break;
+            case 2: 
+                    title.textContent = 'ЛЕДНИКОВЫЙ ПЕРИОД';
+                    genre.textContent = 'КОМЕДИЯ';
+                    descrab.textContent = 'Приключения мамонта, льва, ленивца и других обитателей ледникового периода';
+                    poster.style.backgroundImage = 'url("img/cartoons.jpg")';
+                    break;
+            case 3: 
+                    title.textContent = 'КОРОЛЬ И ШУТ';
+                    genre.textContent = 'ПАНК-РОК';
+                    descrab.textContent = 'САМАЯ КРУТАЯ ГРУППА ОТЕЧЕСТВЕННОГО ПАНК-РОКА';
+                    poster.style.backgroundImage = 'url("img/clips.jpg")';
+                    break;
+        }        
+    }
+    changeTab(0);    
 });
